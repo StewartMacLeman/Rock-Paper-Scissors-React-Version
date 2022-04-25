227c9b5a72e1b85f8573b8b152c972f75cd92336
Stewart MacLeman <stewart_macleman@hotmail.com>
2022-04-25 22:37:45 +0100

    Style updates.

--- src/components/game_area/BestOfButtons.js
+++ src/components/game_area/BestOfButtons.js
@@ -6,10 +6,10 @@ const BestOfButtons = (props) => {
       <h2>Select a best-out-of 3 or 5 Game.</h2>
       <div className="bestOutOfButtons">
         <button type="button" value="3" onClick={props.typeOfGame}>
-          Best of 3
+          Best-out-of 3
         </button>
         <button type="button" value="5" onClick={props.typeOfGame}>
-          Best of 5
+          Best-out-of 5
         </button>
       </div>
     </div>
--- src/components/game_area/ImagesCont.js
+++ src/components/game_area/ImagesCont.js
@@ -4,20 +4,20 @@ const ImagesCont = (props) => {
   return (
     <div className="gameArea">
       <div className="sideContainer">
-        <h2 className={!props.showScores ? "hide" : ""}>
+        <h3 className={!props.showScores ? "hide" : ""}>
           Player's Score:
           <span>{` ${props.playNumerator}`}{props.dominator}</span>
-        </h2>
+        </h3>
         <div className={props.playerImage}></div>
       </div>
 
       <div className="divider"></div>
 
       <div className="sideContainer">
-        <h2 className={!props.showScores ? "hide" : ""}>
+        <h3 className={!props.showScores ? "hide" : ""}>
           Computer's Score:
           <span>{` ${props.compNumerator}`}{props.dominator}</span>
-        </h2>
+        </h3>
         <div className={props.computerImage}></div>
       </div>
     </div>
